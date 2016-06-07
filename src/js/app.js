98f01ede574642215958a3184188875eb8ee5b84
itsaso <itsaso.altuna@gmail.com>
2016-06-07 13:18:50 +0200

    menu esqueleto

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
     
-    function siguiente() {
-      var actual = $('li');
-      var next = actual.next();
+	$( ".btn-anim-three" ).on( "click", function() {
+      $(this).toggleClass( "closed" );
+      $('.menu-mobile').toggleClass( "closed" );
+    });
+    
+    // function siguiente() {
+    //   var actual = $('li');
+    //   var next = actual.next();
 
-      $(window).scrollTo(next,400);
-    };
+    //   $(window).scrollTo(next,400);
+    // };
 
-    $('#to-next').click(siguiente);
+    // $('#to-next').click(siguiente);
 
 });
